1691914fdb5d91c2f4a18b26c13df7abf7de043d
Ov3r55 <51037394+Ov3r55@users.noreply.github.com>
2019-05-26 10:38:46 +0200

    inde.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Ov3r'Music, +help");
+    console.log("Le Bot a bienn été connécté");
+});
+
+bot.login("NTgyMDEwMDYwNDIzNTYxMjE2.XOpMQQ.s4fRBoJXcSj6L6AVlUw1HCNdBbM")
